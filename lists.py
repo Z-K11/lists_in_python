e94488a004a32d33bbc9beaa245dbfd37c7728ca
Z-K11 <zulqarnainbazidkhail@gmail.com>
2025-02-20 13:04:27 +0500

    demonstrate how to copy and clone lists

--- lists.py
+++ lists.py
@@ -52,3 +52,22 @@ str = 'Dr. Adil Eng. Sajid Eng. Irfan'
 print (str.split())
 # We can use split function to split the string into a list using our own splitting character 
 print(str.split("Eng."))
+
+# Copy (copy by reference) the list A
+
+A = ["hard rock", 10, 1.2]
+B = A
+print('A:', A)
+print('B:', B)
+
+# Examine the copy by reference
+
+print('B[0]:', B[0])
+A[0] = "banana"
+print('B[0]:', B[0])
+# Clone (clone by value) the list A
+print('Cloning by value')
+B = A[:]
+print('B[0]:', B[0])
+A[0] = "hard rock"
+print('B[0]:', B[0])
