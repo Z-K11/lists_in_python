cd25b068a013d93ce7361673a8e201ba17eda7df
Z-K11 <zulqarnainbazidkhail@gmail.com>
2025-02-20 12:19:25 +0500

    Initial Commit

---
+++ lists.py
@@ -0,0 +1,18 @@
+'''Imagine we recieived album recommendations from a friend and compiled all of 
+the recommendations into a table, with specific information about each album.
+We have one row for each movie and several columns including 
+->Artist 
+->Album
+->Released year 
+->length min sec
+->Genre 
+->Music recording saltes 
+->Calimed sales 
+->Release year 
+->Soundtrack 
+->Rating of friend 
+'''
+
+# create a list 
+list = ['The Body Guard','7.0','1992']
+print(list)
