5c9434bc286fbdd96c5b4917f6e00dc0c64f0549
Z-K11 <zulqarnainbazidkhail@gmail.com>
2025-02-20 12:28:43 +0500

    demonstrate negative and positive indexing in lists

--- lists.py
+++ lists.py
@@ -16,3 +16,7 @@ We have one row for each movie and several columns including
 # create a list 
 list = ['The Body Guard','7.0','1992']
 print(list)
+# Using negative indexing to print the elements in asscending order 
+print(f"The first lement of the list \nusing negative index {list[-3]} \nusing positive indexing {list[0]}")
+print(f"The second lement of the list \nusing negative index {list[-2]} \nusing positive indexing {list[1]}")
+print(f"The third lement of the list \nusing negative index {list[-1]} \nusing positive indexing {list[2]}")
